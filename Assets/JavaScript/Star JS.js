28642525f8ca4910bc1427e877c2827fcb0a7427
Luis <salazarluis0119@gmail.com>
2018-02-19 22:04:15 -0600

    going up

---
+++ Assets/JavaScript/Star JS.js
@@ -0,0 +1,22 @@
+// 4 Objects. 
+var A = {
+    name:"Obi-Wan Kenobi"
+};
+
+var B = {
+    name:"Luke Skywalker"
+};
+
+var C = {
+    name:"Darth Sidious"
+};
+
+var D = {
+    name: "Darth Maul"
+};
+// 4 Objects. 
+
+
+
+
+
